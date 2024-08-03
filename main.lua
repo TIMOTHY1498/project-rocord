a58c1ec0979dc201f64d92596a7848832f316d56
Timothy B <74752830+TIMOTHY1498@users.noreply.github.com>
2024-08-03 17:06:46 +0700

    Version 0.1 - main.lua

---
+++ main.lua
@@ -0,0 +1,58 @@
+local httpservice = game.HttpService
+local url = "paste your webhook token or url here..."
+
+--[[
+// timblox-discblox-api
+
+This project is made by @timothy1498
+Credit me before using it on your game
+]]--
+
+
+game.Players.PlayerAdded:Connect(function(plr)
+	local datajoin = {
+		["content"] = plr.Name.." was joined the game",
+		["username"] = "[SYSTEM]"
+	}
+	httpservice:RequestAsync({
+		Url = url, --// plr.Name was joined the game
+		--// join link
+		Method = "POST",
+		Headers = {
+			["Content-Type"] = "application/json", --// JSON Only!
+		},
+		Body = httpservice:JSONEncode(datajoin),
+	})
+	
+	plr.Chatted:connect(function(msg)
+		local data = {
+			["content"] = msg,
+			["username"] = plr.Name
+		}
+		httpservice:RequestAsync({
+			Url = url,
+			Method = "POST",
+			Headers = {
+				["Content-Type"] = "application/json", --// JSON Only!
+			},
+			Body = httpservice:JSONEncode(data),
+		})
+	end)
+end)
+
+game.Players.PlayerRemoving:Connect(function(plr)
+	local datajoin = {
+		["content"] = plr.Name.." was left the game",
+		["username"] = "[SYSTEM]"
+	}
+	httpservice:RequestAsync({
+		Url = url, --// plr.Name was left the game
+		Method = "POST",
+		Headers = {
+			["Content-Type"] = "application/json", --// JSON Only!
+		},
+		Body = httpservice:JSONEncode(datajoin),
+	})
+end)
+
+print("timblox-discblox-api (Made with love by PakTimoGus124 (timothy1498_boi))")
